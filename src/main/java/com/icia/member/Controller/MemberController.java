ff7d98077864e6644e8876e9c2b9d514e08a8958
Dbeehe <cjh123456a@naver.com>
2023-10-10 10:28:44 +0900

    회원프로젝트 1. 회원가입

---
+++ src/main/java/com/icia/member/Controller/MemberController.java
@@ -0,0 +1,29 @@
+package com.icia.member.Controller;
+
+import com.icia.member.dto.MemberDTO;
+import com.icia.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/save")
+    public String saveForm(){
+        return "memberPages/memberSave";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute MemberDTO memberDTO){
+        memberService.save(memberDTO);
+        return "memberPages/memberLogin";
+    }
+}
---
+++ src/main/java/com/icia/member/entity/MemberEntity.java
@@ -0,0 +1,59 @@
+package com.icia.member.entity;
+
+import com.icia.member.dto.MemberDTO;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter(AccessLevel.PRIVATE)
+@Table(name = "member_table")
+public class MemberEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = (200),nullable = false)
+    private String memberEmail;
+
+    @Column(length = (200),nullable = false)
+    private String memberPassword;
+
+    @Column(length = (200),nullable = false)
+    private String memberName;
+
+    @Column(length = (200),nullable = false)
+    private String memberMobile;
+
+    @Column(length = (200),nullable = false)
+    private String memberBirth;
+
+    @Column
+    private int fileAttached;
+
+
+
+
+    public static MemberEntity toSaveEntity(MemberDTO memberDTO){
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        memberEntity.setMemberMobile(memberDTO.getMemberMobile());
+        memberEntity.setMemberBirth(memberDTO.getMemberBirth());
+        memberEntity.setFileAttached(0);
+        return memberEntity;
+    }
+
+}
+
+
+
+
+
+
+
+
